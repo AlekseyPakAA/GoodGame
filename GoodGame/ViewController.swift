b25eb64205a4554f0ffd7ce15b609c69a48830dd
Alexey Pak <alexey.pak@umbrella-web.com>
2018-03-08 21:34:22 +0300

    Initial Commit

---
+++ GoodGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodGame
+//
+//  Created by alexey.pak on 08/03/2018.
+//  Copyright © 2018 alexey.pak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
