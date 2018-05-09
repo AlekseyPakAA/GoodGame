b3ab7578e2e80f3c5436d31b220c23d00cd193c3
aleksey_pak_aa <aleksey.pakaa@gmail.com>
2018-05-09 15:30:31 +0300

    --

--- Podfile
+++ Podfile
@@ -1,5 +1,6 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
+inhibit_all_warnings!
 
 target 'GoodGame' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
@@ -10,5 +11,6 @@ target 'GoodGame' do
   pod 'AlamofireObjectMapper'
   
   pod 'SwiftWebSocket'
+  pod 'NextGrowingTextView'
 
 end
--- Pods/Target Support Files/Pods-GoodGame/Pods-GoodGame-frameworks.sh
+++ Pods/Target Support Files/Pods-GoodGame/Pods-GoodGame-frameworks.sh
@@ -136,12 +136,14 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AlamofireObjectMapper/AlamofireObjectMapper.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NextGrowingTextView/NextGrowingTextView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftWebSocket/SwiftWebSocket.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/AlamofireObjectMapper/AlamofireObjectMapper.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/NextGrowingTextView/NextGrowingTextView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ObjectMapper/ObjectMapper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SwiftWebSocket/SwiftWebSocket.framework"
 fi
