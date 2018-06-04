eb02a4a9167d459430faa2dcf4c9d11cdaca235c
aleksey_pak_aa <alexey.pak@umbrella-web.com>
2018-06-04 11:22:58 +0300

    --

--- GoodGame/Modules/Chat/Subviews/ChatMessageCell/ChatMessageCell.swift
+++ GoodGame/Modules/Chat/Subviews/ChatMessageCell/ChatMessageCell.swift
@@ -11,66 +11,22 @@ import Kingfisher
 
 class ChatMessageCell: ASCellNode {
 
-	var titleTextNode: ASTextNode  = ASTextNode()
-	var descriptionTextNode: ASTextNode  = ASTextNode()
+	var textNode: ASTextNode  = ASTextNode()
 
 	override init() {
 		super.init()
 
 		automaticallyManagesSubnodes = true
 		selectionStyle = .none
-
-		titleTextNode.maximumNumberOfLines = 1
 	}
 
 	func configure(model: ChatMessageCellViewModel) {
-		let attributes: [NSAttributedStringKey: Any] = [NSAttributedStringKey.font: UIFont.preferredFont(forTextStyle: .headline)]
-		titleTextNode.attributedText = NSAttributedString(string: model.title, attributes: attributes)
-
-		guard let expression = try? NSRegularExpression(pattern: ":([A-z0-9])+:") else { return }
-
-		let matches: [NSTextCheckingResult] = {
-			let nsrange = NSRange(location: 0, length: model.description.count)
-			return expression.matches(in: model.description, range: nsrange).sorted(by: {lhf, rhf in
-				return lhf.range.location > rhf.range.location
-			})
-		}()
-
-		let attributedDescription = NSMutableAttributedString(string: model.description)
-		print("\(model.title):\(model.description)")
-		for match in matches {
-			guard let range = Range<String.Index>(match.range, in: model.description) else {
-				break
-			}
-
-			let smileURL: URL? = {
-				let smileName = model.description[range].replacingOccurrences(of: ":", with: "")
-				return SmilesManager.shared.urlForSmileWith(name: smileName)
-			}()
-
-			if let smileURL = smileURL {
-				let attachment = NSTextAttachment()
-				guard let data = try? Data(contentsOf: smileURL), let image = UIImage(data: data) else { return }
-				attachment.image = image
-				attributedDescription.replaceCharacters(in: match.range, with: NSAttributedString(attachment: attachment))
-			}
-		}
-		print("\(model.title):\(attributedDescription.string)")
-		descriptionTextNode.attributedText = attributedDescription
+		textNode.attributedText = model.text
 	}
 
 	override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-		let spec = ASStackLayoutSpec()
-
-		spec.direction = .vertical
-		spec.alignItems = .start
-		spec.spacing = 8.0
-
-		spec.children = [titleTextNode, descriptionTextNode]
-
-		let insets = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
-
-		return ASInsetLayoutSpec(insets: insets, child: spec)
+		let insets = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
+		return ASInsetLayoutSpec(insets: insets, child: textNode)
 	}
 
 }
--- GoodGame/Modules/Chat/Subviews/ChatMessageCell/ChatMessageCellViewModel.swift
+++ GoodGame/Modules/Chat/Subviews/ChatMessageCell/ChatMessageCellViewModel.swift
@@ -5,22 +5,59 @@
 //  Created by alexey.pak on 02/04/2018.
 //  Copyright © 2018 alexey.pak. All rights reserved.
 //
+import UIKit
 
 struct ChatMessageCellViewModel: Equatable {
 
-    let id: Int
+	let id: Int
+	let text = NSMutableAttributedString()
 
-    let title: String
-    let description: String
+	fileprivate let font = UIFont.preferredFont(forTextStyle: .body)
 
-    init(message: IncomingMessageChatMessage) {
-        id = message.id
-        title = message.userName
-        description = message.text
-    }
+	init(message: IncomingMessageChatMessage) {
+		id = message.id
 
-    static func == (lhs: ChatMessageCellViewModel, rhs: ChatMessageCellViewModel) -> Bool {
-        return lhs.id == rhs.id
-    }
+		let attributes: [NSAttributedStringKey: Any] = [NSAttributedStringKey.font: font]
+		let authorAttributedString = NSAttributedString(string: "\(message.userName): ", attributes: attributes)
+
+		guard let expression = try? NSRegularExpression(pattern: ":([A-z0-9])+:") else { return }
+
+		let matches: [NSTextCheckingResult] = {
+			let nsrange = NSRange(location: 0, length: message.text.count)
+			return expression.matches(in: message.text, range: nsrange).sorted(by: {lhf, rhf in
+				return lhf.range.location > rhf.range.location
+			})
+		}()
+
+		let descriptionAttributedString = NSMutableAttributedString(string: message.text, attributes: attributes)
+		for match in matches {
+			guard let range = Range<String.Index>(match.range, in: message.text) else {
+				break
+			}
+
+			let smileURL: URL? = {
+				let smileName = message.text[range].replacingOccurrences(of: ":", with: "")
+				return SmilesManager.shared.urlForSmileWith(name: smileName)
+			}()
+
+			if let smileURL = smileURL {
+				let attachment = NSTextAttachment()
+				guard let data = try? Data(contentsOf: smileURL), let image = UIImage(data: data) else { return }
+				attachment.image = image
+
+				let offsetY = (font.capHeight - image.size.height) / 2
+				attachment.bounds = CGRect(x: 0.0, y: offsetY, width: image.size.width, height: image.size.height)
+
+				descriptionAttributedString.replaceCharacters(in: match.range, with: NSAttributedString(attachment: attachment))
+			}
+		}
+
+		text.append(authorAttributedString)
+		text.append(descriptionAttributedString)
+	}
+
+	static func == (lhs: ChatMessageCellViewModel, rhs: ChatMessageCellViewModel) -> Bool {
+		return lhs.id == rhs.id
+	}
 
 }
